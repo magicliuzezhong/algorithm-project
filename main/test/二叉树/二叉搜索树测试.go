fcad08ea16a8f9e12a10a90c9badc480a63b2f41
刘泽忠 <liuzezhong@cloud-ark.com>
2021-08-31 11:34:58 +0800

    测试下提交代码问题

--- main/test/二叉树/二叉搜索树测试.go
+++ main/test/二叉树/二叉搜索树测试.go
@@ -6,7 +6,6 @@
 //
 package main
 
-import "fmt"
 import . "../../../model"
 
 func main() {
@@ -19,12 +18,12 @@ func main() {
 	tree.Remove(5)
 	PrintTree(tree.Root)
 
-	var c = 2 ^ 3 // 0010 ^ 0011 = 0001
-	var d = 2 ^ 7 // 0010 ^ 0111 = 0101
-	var e = 3 << 1 // 0011 << 1 = 0110 = 6
-	var f = 3 << 2 // 0011 << 2 = 1100 = 12
-	fmt.Println(c) // 0001  =  1
-	fmt.Println(d) // 0101  =  5
-	fmt.Println(e) // 0101  =  6
-	fmt.Println(f) // 0101  =  12
+	//var c = 2 ^ 3 // 0010 ^ 0011 = 0001
+	//var d = 2 ^ 7 // 0010 ^ 0111 = 0101
+	//var e = 3 << 1 // 0011 << 1 = 0110 = 6
+	//var f = 3 << 2 // 0011 << 2 = 1100 = 12
+	//fmt.Println(c) // 0001  =  1
+	//fmt.Println(d) // 0101  =  5
+	//fmt.Println(e) // 0101  =  6
+	//fmt.Println(f) // 0101  =  12
 }
